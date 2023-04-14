1fb5896ea62fa790e30b6e15a053cc9abaf09ae3
BrokenShell <webmaster@sharpdesigndigital.com>
2023-04-14 07:21:45 -0700

    updates Form inputs

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as file:
     next(file)
     description = file.read()
 
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 API = FastAPI(
     title='Outreach API',
     description=description,
