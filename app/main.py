edba7cec162f02ab38c67beaeed4bd21d7e97feb
BrokenShell <webmaster@sharpdesigndigital.com>
2023-04-18 10:38:01 -0700

    updates email to use templates

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as file:
     next(file)
     description = file.read()
 
-VERSION = "0.0.10"
+VERSION = "0.0.11"
 API = FastAPI(
     title='Outreach API',
     description=description,
@@ -94,14 +94,13 @@ async def outreach(your_name: str,
         "https://api.mailgun.net/v3/mail.bloomtech.com/messages",
         auth=("api", os.getenv("MAILGUN_API_KEY")),
         headers={
-            "X-Mailgun-Variables": '{"outreach_message": cold_outreach, "contact": contacts}',
+            "X-Mailgun-Variables": {"outreach_message": cold_outreach, "contact": contacts},
         },
         data={
             "from": "Outreach Generator <support@bloomtech.com>",
             "to": f"{your_name} <{your_email}>",
             "subject": f"Custom Outreach for {company}",
             "template": "custom_outreach",
-
         }
     )
     return {
